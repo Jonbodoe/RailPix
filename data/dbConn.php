7816fa3abbf6dc38ef2e08d547019a9e6133bab8
Jonbodoe <le0220@mail.philau.edu>
2019-12-15 17:35:22 -0500

    db connection edits

--- data/dbConn.php
+++ data/dbConn.php
@@ -6,7 +6,16 @@ $username = 'root';
 $pw = 'root';
 $port = 8889;
 
+// $hostname = 'localhost';
+// $dbname = 'jonathan_railpix';
+// $username = 'jonathan_le';
+// $pw = 'Skyline1997';
+
+// If connecting to cpanel, use this instead if locally, use the first one.
+
 $connect = new mysqli($hostname, $username, $pw, $dbname, $port); 
+
+// remove $port if connecting to cpanel
 
 if($connect->connect_error) {
     die("connection failed : " . $connect->connect_error);
